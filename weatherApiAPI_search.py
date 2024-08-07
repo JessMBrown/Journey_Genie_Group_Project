0b478974fc169c44fceb9cb9b6edd305e3c2e170
Jessica Brown <j.p.donovan.brown@gmail.com>
2024-08-07 23:57:54 +0100

    started weather API file and added .env with API keys

---
+++ weatherApiAPI_search.py
@@ -0,0 +1,3 @@
+base_url = "https://api.weatherapi.com/v1"
+history_endpoint = "/history.json?q=London&dt=2023-12-31&end_dt=2024-01-05&key="
+future_endpoint = "/future.json?q=London&dt=2024-08-22&key="
