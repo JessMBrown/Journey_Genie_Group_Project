bacc65f5b30140f7a93d68d6285d5fe5cf818413
Joana <joana.afonso1@outlook.com>
2024-08-13 14:47:00 +0100

    modified tailored_trip

--- Joana_main.py
+++ Joana_main.py
@@ -6,56 +6,60 @@ from utils import UserInputCheck
 import random
 
 
-# def knows_destination():
-#     chosen_country = input("Please, enter the name of the country: ")  # modify to make it a fixed list of countries
-#     #  Make a list of cities to chose from
-#     #  Call weather
-#     #  call hotels function to get list of hotels
-#     #  call activities function to make suggestions find_activities(city)
-#     #  call email function
-#
-#
-# def tailored_trip():
-#     # take user input on what type of holiday he would like
-#     # create code to get countries and get user to choose one
-#     # call find cities to display list of cities
-#     # call weather
-#     # call hotels function to display hotels
-#     # call activities function to display activities
-#     # call email function to get email sent
-#     pass
-#
-# def take_me_anywhere():
-#     #  create code to random to call find country
-#     random_countries = random.choices(country_list, k=5) # need to replace with name of list from db custom API
-#
-#     while True:
-#         good_selection = input(f"Fancy any of these countries? {random_countries} Y/N ").lower().strip()
-#         if good_selection == 'y':
-#             country_choice = input('Please enter the name of the country: ')
-#             break
-#         elif good_selection == 'n':
-#             continue
-#         else:
-#             print('Invalid answer! Please type Y or N ')
-#
-#     # call cities
-#     city_choice = find_cities(country_choice)
-#     # call weather
-#     find_weather(city_choice)
-#     # call hotels
-#     find_hotels(city_choice)
-#     # call activities
-#     find_activities(city_choice)
-#     # call email
-#     get_email()
-#     #(#  get favourites)
+def knows_destination(): # KAREN
+    chosen_country = input("Please, enter the name of the country: ")  # modify to make it a fixed list of countries
+    #  Make a list of cities to chose from
+    #  Call weather
+    #  call hotels function to get list of hotels
+    #  call activities function to make suggestions find_activities(city)
+    #  call email function
 
-def find_cities(country): # or should it be find countries and cities?
+
+def tailored_trip(): # JOANA
+    # city/country function to display countries based on what he's looking for
+    country_choice = find_cities(preferences)
+    city_choice = find_cities(country_choice)
+    # call weather
+    find_weather(city_choice, start_date, end_date)
+    # call hotels
+    find_hotels(city_choice, start_date, end_date, num_adults, num_children)
+    # call activities
+    find_activities(city_choice)
+    # call email
+    get_email()
     pass
-def find_weather(city, start_date, end_date):
+
+def take_me_anywhere(): # JOANA
+    #  create code to random to call find country
+    random_countries = random.choices(country_list, k=5) # need to replace with name of list from db custom API
+
+    while True:
+        good_selection = input(f"Fancy any of these countries? {random_countries} Y/N ").lower().strip()
+        if good_selection == 'y':
+            country_choice = input('Please enter the name of the country: ')
+            break
+        elif good_selection == 'n':
+            continue
+        else:
+            print('Invalid answer! Please type Y or N ')
+
+    # call cities
+    city_choice = find_cities(country_choice)
+    # call weather
+    find_weather(city_choice, start_date, end_date)
+    # call hotels
+    find_hotels(city_choice, start_date, end_date, num_adults, num_children)
+    # call activities
+    find_activities(city_choice)
+    # call email
+    get_email()
+    #(#  get favourites)
+
+def find_cities(country): # or should it be find countries and cities? OLI
     pass
-def find_hotels(city, num_adults, num_children):
+def find_weather(city, start_date, end_date): JESS
+    pass
+def find_hotels(city, num_adults, num_children): NADIA
     pass
 def find_activities(city):
     input_check = UserInputCheck()
@@ -151,31 +155,29 @@ def find_activities(city):
         except Exception:
             print('Error')
 
-def get_email():
+def get_email(): #OLI
     pass
 print(find_activities('London'))
-# def save_chosen_suggestions():
-#     pass
-#
-# def main():
-#     input_check = UserInputCheck()
-#     # Welcoming user and getting some basic details
-#     print("Hello! Welcome to Journey Genie! Let's start prepping your next holiday!")
-#     start_date = input("First, please enter the start date for your holiday (YYYY-MM-DD): ")
-#     end_date = input("Now, please enter the end date for your holiday (YYYY-MM-DD): ")
-#     num_adults = input('How many adults will you be? ')
-#     num_children = input("How many children? ")
-#     knows_where = input_check.get_input("Do you know which country you'd like to go to? Y/N ")
-#
-#     if knows_where == 'y':
-#         knows_destination()
-#     elif knows_where == 'n':
-#         wants_random = input_check.get_input("No worries! We're here to help! Would you like us to make a random guess of a nice holiday place for you? Y/N ")
-#         if wants_random == 'y':
-#             take_me_anywhere()
-#         elif wants_random == 'n':
-#             print("Ok! Let's tailor a nice holiday for you!")
-#             tailored_trip()
+
+def main(): # JOANA
+    input_check = UserInputCheck()
+    # Welcoming user and getting some basic details
+    print("Hello! Welcome to Journey Genie! Let's start prepping your next holiday!")
+    start_date = input("First, please enter the start date for your holiday (YYYY-MM-DD): ")
+    end_date = input("Now, please enter the end date for your holiday (YYYY-MM-DD): ")
+    num_adults = input('How many adults will you be? ')
+    num_children = input("How many children? ")
+    knows_where = input_check.get_input("Do you know which country you'd like to go to? Y/N ")
+
+    if knows_where == 'y':
+        knows_destination()
+    elif knows_where == 'n':
+        wants_random = input_check.get_input("No worries! We're here to help! Would you like us to make a random guess of a nice holiday place for you? Y/N ")
+        if wants_random == 'y':
+            take_me_anywhere()
+        elif wants_random == 'n':
+            print("Ok! Let's tailor a holiday for you!")
+            tailored_trip()
 
 
 if __name__ == "__main__":
