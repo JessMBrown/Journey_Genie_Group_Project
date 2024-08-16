093c1d469dedbed2a5321d5c13bc97558bc5515b
Joana <joana.afonso1@outlook.com>
2024-08-16 07:51:30 +0100

    add utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 class UserInputCheck:
     def __init__(self):
@@ -19,3 +20,13 @@ class UserInputCheck:
 
         return kinds
 
+def valid_date(prompt):
+    date_str = input(prompt)
+    try:
+        date_obj = datetime.strptime(date_str, "%d-%m-%Y")
+        if date_obj.date() < datetime.today().date():
+            print("The check-in date must be today or later. Please enter a valid date.")
+        else:
+            return date_obj
+    except ValueError:
+        print("Invalid date format. Please enter the date in dd-mm-yyyy format.")
