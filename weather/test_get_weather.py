05e82e60eed1822484a325780982c04c012b6e20
Jessica Brown <j.p.donovan.brown@gmail.com>
2024-08-21 00:55:22 +0100

    changed unit tests

--- weather/test_get_weather.py
+++ weather/test_get_weather.py
@@ -25,7 +25,7 @@ class TestFindWeatherFunction(unittest.TestCase):
     def test_weather_future_endpoint_valid(self):
         self.input_weather_data_location = "London"
         self.input_weather_data_start_date = '2024-10-01'
-        self.input_weather_data_end_date = '2024-10-03'
+        self.input_weather_data_end_date = '2024-10-01'
         self.expect = [{'average_temp': 14.5, 'date': '2024-10-01'}]
         self.assertEqual(self.expect, get_weather.find_weather(self.input_weather_data_location,
                                                         self.input_weather_data_start_date,
@@ -36,7 +36,7 @@ class TestAddDaysFunction(unittest.TestCase):
 
     def test_add_days_valid_1(self):
         self.number_to_add = 1
-        self.expected = '2024-08-16'
+        self.expected = '2024-08-22'
         self.assertEqual(self.expected, str(get_weather.add_days(self.number_to_add)))
 
 
@@ -44,7 +44,7 @@ class TestSubtractDaysFunction(unittest.TestCase):
 
     def test_subtract_days_valid_1(self):
         self.number_to_subtract = 1
-        self.expected = '2024-08-14'
+        self.expected = '2024-08-20'
         self.assertEqual(self.expected, str(get_weather.subtract_days(self.number_to_subtract)))
 
 
