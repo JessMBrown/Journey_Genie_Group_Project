f242ca1f9653207e02a6593c82c97816f1505ccc
Nadia Rehman <itsnadiarehman@gmail.com>
2024-08-20 20:21:30 +0100

    adding links for the hotels

--- get_hotels.py
+++ get_hotels.py
@@ -1,5 +1,6 @@
-from hotels_api import fetch_price, fetch_hotels_with_filters, search_cities
+from hotels_api import fetch_price, fetch_hotels_with_filters, search_cities, fetch_hotel_details_with_links
 from hotels_utils import get_valid_dates
+import urllib.parse
 
 # searching for a city to go to
 def city_search():
@@ -17,125 +18,123 @@ def city_search():
         # if nothing is found, a prompt for the user to try again
         print(f"No locations found matching the name '{search}'. Try again.")
 
-def main():
-    city_selected = False
-    while not city_selected:
-        # search for the desired city
-        chosen_option = city_search()
-
-        # ensuring valid dates from utils file
-        check_in_date, check_out_date = get_valid_dates()
-
-        # handle number of people input
-        adults_valid = False
-        while not adults_valid:
-            try:
-                # number of people
-                adults = int(input("Enter the number of people (max 4 per booking): "))
-                if adults > 4:
-                    print("The maximum number of people allowed within a single booking is 4. Please try again.")
-                else:
-                    adults_valid = True
-            except ValueError:
-                print("Invalid input. Please enter a valid number.")
-
-        # handle number of rooms input
-        rooms_valid = False
-        while not rooms_valid:
-            try:
-                # number of rooms
-                rooms = int(input("Enter the number of rooms (max 3): "))
-                if rooms > 3:
-                    print("The maximum number of rooms allowed is 3. Please enter a valid number of rooms.")
-                elif adults > rooms * 2:
-                    print("The number of people exceeds the 2-person capacity per room. Please enter more rooms.")
-                else:
-                    rooms_valid = True
-            except ValueError:
-                print("Invalid input. Please enter a valid number.")
-
-        # getting the hotels in the desired city
-        location_id = chosen_option['id']
-
-        # show the filter options
-        available_filters = [
-            "center", "luxury", "3-stars", "4-stars", "5-stars",
-            "business", "romantic", "family", "pets", "pool"
-        ]
-        print("\nAvailable Filters:")
-        for index, filter_name in enumerate(available_filters, 1):
-            print(f"{index}. {filter_name.capitalize()}")
-
-        # getting the filters selected by the user
-        selected_filters = input("Enter the numbers corresponding to the filters you'd like to apply (comma-separated): ")
-        selected_filters = [available_filters[int(index) - 1] for index in selected_filters.split(",")]
-
-        # fetching hotels based on location and filters
-        filtered_hotels = fetch_hotels_with_filters(location_id, check_in_date, check_out_date, selected_filters)
-
-        if filtered_hotels:
-            city_selected = True
-            hotel_prices = []
-            for hotel in filtered_hotels:
-                try:
-                    # getting the prices of all the hotels
-                    price = fetch_price(hotel_id=hotel['hotel_id'], check_in=check_in_date, check_out=check_out_date,
-                                        rooms=rooms, adults=adults)
-                    if price is not None:
-                        # add hotel and price to list
-                        hotel_prices.append({
-                            'hotelName': hotel['name'],
-                            'hotelId': hotel['hotel_id'],
-                            'price': price
-                        })
-                    else:
-                        hotel_prices.append({
-                            'hotelName': hotel['name'],
-                            'hotelId': hotel['hotel_id'],
-                            'price': "Price information unavailable"
-                        })
-                except Exception as e:
-                    print(f"An error occurred while fetching price for hotel '{hotel['name']}': {e}")
-                    hotel_prices.append({
-                        'hotelName': hotel['name'],
-                        'hotelId': hotel['hotel_id'],
-                        'price': "Price information unavailable"
-                    })
-
-            if hotel_prices:
-                # sort hotels based on price and handling when the info is not available
-                hotel_prices.sort(
-                    key=lambda x: x['price'] if isinstance(x['price'], (int, float)) else (
-                        float('inf') if x['price'] == "Price unavailable" else x['price'])
-                )
-
-                # display hotel and price info
-                print(f"\nHotels in {chosen_option['cityName']} with the filters: {', '.join(selected_filters).capitalize()} (by price asc):")
-                for index, hotel in enumerate(hotel_prices):
-                    price_display = f"{hotel['price']} GBP" if isinstance(hotel['price'], (int, float)) else hotel['price']
-                    print(f"{index + 1}. {hotel['hotelName']} - Total Price: {price_display}")
-
-                hotel_selected = False
-                while not hotel_selected:
-                    try:
-                        # ask user to select a hotel from the list
-                        hotel_choice = int(input("\nEnter the number corresponding to the hotel you would like to select: "))
-                        if 1 <= hotel_choice <= len(hotel_prices):
-                            chosen_hotel = hotel_prices[hotel_choice - 1]
-                            hotel_id = chosen_hotel['hotelId']
-                            # display their selection
-                            print(f"\nYou selected: {chosen_hotel['hotelName']} - Total Price: {price_display}")
-                            hotel_selected = True
-                        else:
-                            print("Invalid choice. Try again.")
-                    except ValueError:
-                        print("Invalid input. Please enter a valid number.")
+# entering the number of people
+def get_number_of_people():
+    while True:
+        try:
+            adults = int(input("Enter the number of people (max 4 per booking): "))
+            if adults > 4:
+                print("The maximum number of people allowed within a single booking is 4. Please try again.")
             else:
-                print("No hotels with valid prices were found. Please try another city or modify your criteria.")
-                city_selected = False
+                return adults
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+# ensuring a valid number of rooms
+def get_number_of_rooms(adults):
+    while True:
+        try:
+            rooms = int(input("Enter the number of rooms (max 3): "))
+            if rooms > 3:
+                print("The maximum number of rooms allowed is 3. Please enter a valid number of rooms.")
+            elif adults > rooms * 2:
+                print("The number of people exceeds the 2-person capacity per room. Please enter more rooms.")
+            else:
+                return rooms
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+# list of filters for user to select
+def get_selected_filters():
+    available_filters = [
+        "center", "luxury", "3-stars", "4-stars", "5-stars",
+        "business", "romantic", "family", "pets", "pool"
+    ]
+    print("\nAvailable Filters:")
+    for index, filter_name in enumerate(available_filters, 1):
+        print(f"{index}. {filter_name.capitalize()}")
+
+    selected_filters = input("Enter the numbers corresponding to the filters you'd like to apply (comma-separated): ")
+    return [available_filters[int(index) - 1] for index in selected_filters.split(",")]
+
+# list of hotels by the criteria and shows price
+def fetch_and_sort_hotels(location_id, check_in_date, check_out_date, selected_filters, rooms, adults):
+    filtered_hotels = fetch_hotels_with_filters(location_id, check_in_date, check_out_date, selected_filters)
+    hotel_prices = []
+
+    for hotel in filtered_hotels:
+        try:
+            price = fetch_price(hotel_id=hotel['hotel_id'], check_in=check_in_date, check_out=check_out_date,
+                                rooms=rooms, adults=adults)
+            hotel_prices.append({
+                'hotelName': hotel['name'],
+                'hotelId': hotel['hotel_id'],
+                'price': price if price is not None else "Price information unavailable"
+            })
+        except Exception as e:
+            print(f"An error occurred while fetching price for hotel '{hotel['name']}': {e}")
+            hotel_prices.append({
+                'hotelName': hotel['name'],
+                'hotelId': hotel['hotel_id'],
+                'price': "Price information unavailable"
+            })
+    # sorting the price by asc order
+    hotel_prices.sort(
+        key=lambda x: x['price'] if isinstance(x['price'], (int, float)) else (
+            float('inf') if x['price'] == "Price unavailable" else x['price'])
+    )
+    return hotel_prices
+
+# link for the hotels
+def display_hotels_with_links(hotel_prices, hotels_with_links, city_name, selected_filters):
+    print(f"\nHotels in {city_name} with the filters: {(', '.join(selected_filters)).capitalize()} (by price asc):")
+
+    for index, hotel in enumerate(hotel_prices):
+        price_display = f"{hotel['price']} GBP" if isinstance(hotel['price'], (int, float)) else hotel['price']
+        print(f"{index + 1}. {hotel['hotelName']} - Total Price: {price_display}")
+
+        hotel_link = "Link not available"
+        for h in hotels_with_links:
+            if h['hotelId'] == hotel['hotelId']:
+                hotel_link = h['link']
+
+        # encoded so special characters can be read in the url
+        hotel_link = urllib.parse.quote(hotel_link, safe=":/?&=")
+        print(f"{hotel_link}")
+
+# user can select a hotel from the options and choice is displayed
+def get_hotel_choice(hotel_prices):
+    while True:
+        try:
+            hotel_choice = int(input("\nEnter the number corresponding to the hotel you would like to select: "))
+            if 1 <= hotel_choice <= len(hotel_prices):
+                chosen_hotel = hotel_prices[hotel_choice - 1]
+                print(f"\nYou selected: {chosen_hotel['hotelName']} - Total Price: {chosen_hotel['price']} GBP")
+                return True
+            else:
+                print("Invalid choice. Try again.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+def main():
+    while True:
+        chosen_option = city_search()
+        check_in_date, check_out_date = get_valid_dates()
+        adults = get_number_of_people()
+        rooms = get_number_of_rooms(adults)
+        selected_filters = get_selected_filters()
+
+        hotel_prices = fetch_and_sort_hotels(chosen_option['id'], check_in_date, check_out_date, selected_filters,
+                                             rooms, adults)
+
+        if hotel_prices:
+            hotels_with_links = fetch_hotel_details_with_links(chosen_option['id'], check_in_date, check_out_date,
+                                                               adults)
+            display_hotels_with_links(hotel_prices, hotels_with_links, chosen_option['cityName'], selected_filters)
+            hotel_selected = get_hotel_choice(hotel_prices)
+            if hotel_selected:
+                break
         else:
-            print("Unfortunately, no hotels were found for the selected criteria. Please try another city.")
-            city_selected = False
+            print("No hotels with valid prices were found. Please try another city or modify your criteria.")
 
 if __name__ == "__main__":
     main()
--- hotels_utils.py
+++ hotels_utils.py
@@ -1,21 +1,28 @@
 from datetime import datetime
+
 # function to validate the check-in and check-out dates
 def get_valid_dates():
-    while True:
+    # today's date
+    today = datetime.today()
+
+    # calculation to find maximum check-in date (1 year and 1 month ahead)
+    max_year = today.year + 1
+    max_month = today.month + 1
+    if max_month > 12:
+        max_month = 1
+        max_year += 1
+
+    max_check_in_date = today.replace(year=max_year, month=max_month)
+
+    check_in_date = None
+    check_out_date = None
+    valid_check_in_date = False
+    valid_check_out_date = False
+
+    # check-in date
+    while not valid_check_in_date:
+        check_in_date_str = input("Enter check-in date (dd-mm-yyyy): ")
         try:
-            # today's date
-            today = datetime.today()
-
-            # calculation to find maximum check-in date (1 year and 1 month ahead)
-            max_year = today.year + 1
-            max_month = today.month + 1
-            if max_month > 12:
-                max_month = 1
-                max_year += 1
-
-            max_check_in_date = today.replace(year=max_year, month=max_month)
-
-            check_in_date_str = input("Enter check-in date (dd-mm-yyyy): ")
             check_in_date = datetime.strptime(check_in_date_str, "%d-%m-%Y")
 
             # making sure user does not write a date in the past
@@ -26,10 +33,18 @@ def get_valid_dates():
             # making sure check in date is within the max
             if check_in_date > max_check_in_date:
                 print(
-                    "The check-in date cannot be later than one year and one month from today. Please enter a valid date.")
+                    "The check-in date cannot be later than one year and one month from today. "
+                    "Please enter a valid date.")
                 continue
 
-            check_out_date_str = input("Enter check-out date (dd-mm-yyyy): ")
+            valid_check_in_date = True
+        except ValueError:
+            print("Invalid check-in date format. Please enter the date in dd-mm-yyyy format.")
+
+    # get and validate check-out date
+    while not valid_check_out_date:
+        check_out_date_str = input("Enter check-out date (dd-mm-yyyy): ")
+        try:
             check_out_date = datetime.strptime(check_out_date_str, "%d-%m-%Y")
 
             # making sure check-out date is after the check-in date
@@ -37,7 +52,8 @@ def get_valid_dates():
                 print("Check-out date must be after the check-in date. Please enter the dates again.")
                 continue
 
-            return check_in_date, check_out_date
-
+            valid_check_out_date = True
         except ValueError:
-            print("Invalid date format. Please enter the date in dd-mm-yyyy format.")
+            print("Invalid check-out date format. Please enter the date in dd-mm-yyyy format.")
+
+    return check_in_date, check_out_date
