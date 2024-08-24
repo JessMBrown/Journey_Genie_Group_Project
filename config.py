7a2d7a006aabf9ef095bf0a99ba9ed07d599721c
Oliwia Polakiewicz <opolakiewicz@gmail.com>
2024-08-25 00:44:32 +0200

    getting rid of my password

--- config.py
+++ config.py
@@ -1,15 +1,7 @@
-# activities_api_key = ''
-# hotels_api_key = ''
-# weather_api_key = ''
-#
-# HOST = "localhost"
-# USER = "root"
-# PASSWORD = ""
+activities_api_key = ''
+hotels_api_key = ''
+weather_api_key = ''
 
 HOST = "localhost"
 USER = "root"
-PASSWORD = "D@mn3ron"
-
-hotels_api_key = "438793230bf5b5f976eb587fb93f532e"
-activities_api_key = "5ae2e3f221c38a28845f05b6163c30aca4c337beba00f5a64b20a267"
-weather_api_key = "dbe4c165ac0a44b8bac160154242408"
+PASSWORD = "password, please"
