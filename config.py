abe9cb38a921f769b4121bf3712d8365d27f9df0
JessMBrown <160969224+JessMBrown@users.noreply.github.com>
2024-08-25 23:26:00 +0100

    Update config.py
    removed keys

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-activities_api_key = '5ae2e3f221c38a28845f05b6e3baf69fbc162e4ddc66896396b6ffcf'
-hotels_api_key = '908419a87b583612651ca29360b13d72'
-weather_api_key = 'ce3a1930b9cf4e26944182911242508'
+activities_api_key = ''
+hotels_api_key = ''
+weather_api_key = ''
 
 HOST = "localhost"
 USER = "root"
-PASSWORD = "LilySue2019!"
+PASSWORD = ""
